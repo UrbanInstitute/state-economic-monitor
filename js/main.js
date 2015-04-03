0abe0986c2cf3449b7109afda6e8f83dcfb4802b
Chartoff <bchartoff@urban.org>
2015-04-03 12:31:48 -0400

    mouseleave shim for IE

--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
+(function() {
+ 
+  // get a reference to the d3.selection prototype,
+  // and keep a reference to the old d3.selection.on
+  var d3_selectionPrototype = d3.selection.prototype,
+      d3_on = d3_selectionPrototype.on;
+ 
+  // our shims are organized by event:
+  // "desired-event": ["shimmed-event", wrapperFunction]
+  var shims = {
+    "mouseenter": ["mouseover", relatedTarget],
+    "mouseleave": ["mouseout", relatedTarget]
+  };
+ 
+  // rewrite the d3.selection.on function to shim the events with wrapped
+  // callbacks
+  d3_selectionPrototype.on = function(evt, callback, useCapture) {
+    var bits = evt.split("."),
+        type = bits.shift(),
+        shim = shims[type];
+    if (shim) {
+      evt = [shim[0], bits].join(".");
+      callback = shim[1].call(null, callback);
+      return d3_on.call(this, evt, callback, useCapture);
+    } else {
+      return d3_on.apply(this, arguments);
+    } 
+  };
+ 
+  function relatedTarget(callback) {
+    return function() {
+      var related = d3.event.relatedTarget;
+      if (this === related || childOf(this, related)) {
+        return undefined;
+      }
+      return callback.apply(this, arguments);
+    };
+  }
+ 
+  function childOf(p, c) {
+    if (p === c) return false;
+    while (c && c !== p) c = c.parentNode;
+    return c === p;
+  }
+ 
+})();
+
+
 /* Modernizr 2.8.3 (Custom Build) | MIT & BSD
  * Build: http://modernizr.com/download/#-mq
  */
@@ -327,7 +375,7 @@ function drawMapFigure(dataID, config, print){
             .attr("class",dataID + " q" + i + "-4")
             .attr("x",keyWidth*i)
             .on("mouseover",function(){ mouseEvent(dataID, {type: "Legend", "class": "q" + (this.getAttribute("x")/keyWidth) + "-4"}, "hover") })
-            .on("mouseout", function(){
+            .on("mouseleave", function(){
               d3.selectAll(".hover").classed("hover",false)
               d3.selectAll(".demphasized").classed("demphasized",false)
               d3.selectAll(".text-highlight").classed("text-highlight",false)
@@ -386,7 +434,7 @@ function drawMapFigure(dataID, config, print){
           .attr("id", function(d) { return "state-outline_" + dataID + "_" + d.id ;})
           .attr("d", path)
           .on("mouseover", function(d){mouseEvent(dataID,d,"hover")})
-          .on("mouseout", function(d){
+          .on("mouseleave", function(d){
               d3.selectAll(".hover").classed("hover",false)
               d3.selectAll(".demphasized").classed("demphasized",false)
               d3.selectAll(".text-highlight").classed("text-highlight",false)
