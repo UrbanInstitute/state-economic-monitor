a2af112019403a9c461316d948400f32b5736501
Ben Chartoff <bchartoff@gmail.com>
2015-11-13 13:15:10 -0500

    more historical tax types

--- reshape_historical.py
+++ reshape_historical.py
@@ -95,7 +95,7 @@ def reshapeMonthly(fileName):
 
 # quarterly = ["housing_historical","state_total_tax_historical"]
 
-quarterly = ["housing_historical","state_total_tax_values_historical"]
+quarterly = ["housing_historical","state_total_tax_values_historical","corp_historical","income_historical","sales_historical"]
 monthly = ["total_employment_historical","gov_employment_historical","unemployment_historical","wages_historical"]
 
 for qFile in quarterly:
