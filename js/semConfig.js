251d3ec58ae95bbea0cd5f8f592c23631ba3fb52
SEM App <bchartoff@gmail.com>
2015-12-21 08:06:06 -0500

    12/20 update

--- js/semConfig.js
+++ js/semConfig.js
@@ -124,7 +124,7 @@ var semConfig = {
 					"unit": "percent change year over year",
 					"unit-type": "percent",
 					"date-updated": "{{excel}}",
-					"breaks": [-3,0,1,2,3,4],
+					"breaks": [-3,0,1,2,3,5],
 					"source": "<a href = 'http://www.bls.gov/news.release/laus.t05.htm'>Bureau of Labor Statistics.</a>"
 				},
 		"AWW":	{
@@ -137,7 +137,7 @@ var semConfig = {
 					"unit": "dollars",
 					"unit-type": "dollar",
 					"date-updated": "{{excel}}",
-					"breaks": [600,700,800,900,1000,1300],
+					"breaks": [650,725,800,875,950,1350],
 					"source": "<a href = 'http://www.bls.gov/sae/home.htm'>Bureau of Labor Statistics.</a>"
 				},
 		"AWWChg":	{
@@ -150,7 +150,7 @@ var semConfig = {
 					"unit": "percent change year over year",
 					"unit-type": "percent",
 					"date-updated": "11/2014",
-					"breaks": [-10,-1,0,2,4,8],
+					"breaks": [-10,-1,0,2,4,10],
 					"source": "<a href = 'http://www.bls.gov/sae/home.htm'>Bureau of Labor Statistics.</a>"
 				},
 		"HPChgYr":	{
