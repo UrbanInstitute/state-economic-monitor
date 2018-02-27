8e36e671ad34cedf8d3c465ba7b73b6f88aa9a15
SEM App <bchartoff@gmail.com>
2018-02-27 17:11:18 -0500

    housing update

--- js/semConfig.js
+++ js/semConfig.js
@@ -163,7 +163,7 @@ var semConfig = {
 					"unit": "percent change year over year",
 					"unit-type": "percent",
 					"date-updated": "11/2014",
-					"breaks": [0,3,5,7,9,12],
+					"breaks": [-1,3,5,8,10,15],
 					"source": "<a href = 'http://www.fhfa.gov/DataTools/Downloads/Pages/House-Price-Index-Datasets.aspx#qpo'>Federal Housing Finance Agency.</a>"
 				},
 		"TOTAL":	{
