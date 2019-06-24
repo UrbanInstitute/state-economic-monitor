e0926eea5bc04accf665462ac7fc7dc6e274a521
SEM App <bchartoff@gmail.com>
2019-06-24 13:40:26 -0400

    employment and earnings update

--- js/semConfig.js
+++ js/semConfig.js
@@ -137,7 +137,7 @@ var semConfig = {
 					"unit": "dollars",
 					"unit-type": "dollar",
 					"date-updated": "{{excel}}",
-					"breaks": [700,800,900,1000,1100,1700],
+					"breaks": [700,800,900,1000,1100,1600],
 					"source": "<a href = 'http://www.bls.gov/sae/home.htm'>Bureau of Labor Statistics.</a>"
 				},
 		"AWWChg":	{
@@ -150,7 +150,7 @@ var semConfig = {
 					"unit": "percent change year over year",
 					"unit-type": "percent",
 					"date-updated": "11/2014",
-					"breaks": [-7,-3,0,1,2,5],
+					"breaks": [-3,0,1,2,3,7],
 					"source": "<a href = 'http://www.bls.gov/sae/home.htm'>Bureau of Labor Statistics.</a>"
 				},
 		"HPChgYr":	{
