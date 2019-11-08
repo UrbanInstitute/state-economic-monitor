68c7bbb87bf06c4d38d964dbb4b4622367ad98d5
Ben Chartoff <bchartoff@gmail.com>
2019-11-08 13:37:12 -0500

    ibid

--- static/js/main.js
+++ static/js/main.js
@@ -3546,7 +3546,7 @@ function initControls(){
 				var args = makeCSV(data, params.indicator, params.unit, filename)
 				var dictionaryFileName = "sem_" + sectionFileName + "_data_dictionary.txt"
 				d3.text("static/data/dictionaries/" + dictionaryFileName).then(function(text) {
-					args["dictionaryText"] = text.replace(/^([^\n\r]*)[\n|\r]+$/g, '\1\r\n');
+					args["dictionaryText"] = text.replace(/^([^\n\r]*)[\n|\r]+$/g, '\1\r\r\n');
 				})
 
 				args["dictionaryFileName"] = dictionaryFileName
